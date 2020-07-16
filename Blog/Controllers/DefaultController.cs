1f7ce845d7d4b2aaefdcae9b2d76ea5672cb441e
corut.furkan <corut.furkan@std.izu.edu.tr>
2020-07-16 14:18:04 +0300

    Proje dosyası ekle.

---
+++ Blog/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
